acbfeb880fa257dc0174113d3a77a9954f74c7c7
RomaLunatic <293658261+RomaLunatic@users.noreply.github.com>
2026-08-12 00:02:38 +0900

    SpriteからImage統一、仮画像取り込み

--- Build/game-enjoy.loader.js
+++ Build/game-enjoy.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 56701203,
+    wasmFileSize: 56701065,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
