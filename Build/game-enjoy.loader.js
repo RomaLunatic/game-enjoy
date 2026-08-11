64265a3241474550121f399b3bab5af1edb48350
RomaLunatic <293658261+RomaLunatic@users.noreply.github.com>
2026-08-12 07:16:35 +0900

    タッチ機能追加

--- Build/game-enjoy.loader.js
+++ Build/game-enjoy.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 56723452,
+    wasmFileSize: 56725484,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
