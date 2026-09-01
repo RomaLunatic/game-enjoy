5692d899e21b9c9b3445a8b63bd165c1171aa25b
RomaLunatic <293658261+RomaLunatic@users.noreply.github.com>
2026-09-02 01:27:35 +0900

    update

--- Build/game-enjoy.loader.js
+++ Build/game-enjoy.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 56725484,
+    wasmFileSize: 60745679,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
